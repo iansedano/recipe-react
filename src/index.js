266b2c44c4add5283a84bff7a1308507730cf8b2
Ian <iansedano@gmail.com>
2021-05-15 21:47:26 +0200

    rendering a list

--- src/index.js
+++ src/index.js
@@ -4,11 +4,23 @@ import "./style.css"
 
 const root = document.getElementById("root");
 
+const details = [
+    {name: "name", value: "Nepal"},
+    {name: "population", value: "28,095,714"},
+    {name: "area", value: "147,516 km2"},
+    {name: "main religion", value: "Hinduism"},
+]
 
 const mainContainer = React.createElement("div", null, 
-    /*SECTION*/React.createElement("div", {className: "section"}, 
+    React.createElement("div", {className: "section"}, 
         React.createElement("h2", null, "Welcome!"),
         React.createElement("h3", null, "Looks like you are a Coder!")
+    ),
+    React.createElement("div", {className: "section"},
+        React.createElement("h2", null, "Some info about a Country"),
+        details.map((detail) => {
+            return React.createElement("h3", null, `${detail.name} - ${detail.value}`)
+        })
     )
     
 )
