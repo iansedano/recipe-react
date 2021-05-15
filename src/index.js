f5da0bc7ba3546d9938427130414df1861b209e6
Ian <iansedano@gmail.com>
2021-05-15 21:21:46 +0200

    children demo

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from "react-dom";
 
 const root = document.getElementById("root");
 
-const element = React.createElement("h1", {}, "Hello World");
 
-ReactDOM.render(element, root);
+const mainContainer = React.createElement("div", null, 
+    React.createElement("h1", null, "Hello World")
+)
+
+console.log(mainContainer)
+
+ReactDOM.render(mainContainer, root);
