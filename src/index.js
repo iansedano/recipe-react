dd800f59e4b1c3d8aa82a7de3f96f890ee662d1d
Ian <iansedano@gmail.com>
2021-05-16 18:08:43 +0200

    rewriting in JSX

--- src/index.js
+++ src/index.js
@@ -4,22 +4,30 @@ import "./style.css"
 
 const users = require('./users.json')
 
+// const mainContainer = React.createElement("div", null, 
+//     users.map((user, i) => {
+//         return React.createElement(
+//             "section",
+//             {className: "section", key: i},
+//             React.createElement("h2", null, `${user.first_name} ${user.last_name}`),
+//             React.createElement("img", {src: user.avatar, height: "100px", width: "100px"}, null),
+//             React.createElement("h3", null, `"${user.preferences.fav_phrases.at_bar}"`)
+//         )
+//     })
+// )
 
-
-const mainContainer = React.createElement("div", null, 
-    users.map((user, i) => {
-        return React.createElement(
-            "section",
-            {className: "section", key: i},
-            React.createElement("h2", null, `${user.first_name} ${user.last_name}`),
-            React.createElement("img", {src: user.avatar, height: "100px", width: "100px"}, null),
-            React.createElement("h3", null, `"${user.preferences.fav_phrases.at_bar}"`)
-        )
-    })
-)
-
-
-
+function MainContainer() {
+    return (
+        users.map((user,i) => {
+            return <div key = {i} className = "section">
+                <h2>{`${user.first_name} ${user.last_name}`}</h2>
+                <img src= {user.avatar} height= "100px" width= "100px"></img>
+                <h3>{`"${user.preferences.fav_phrases.at_bar}"`}</h3>
+            </div>
+        })
+    )
+    
+}
 
 const root = document.getElementById("root");
-ReactDOM.render(mainContainer, root);
+ReactDOM.render(<MainContainer/>, root);
